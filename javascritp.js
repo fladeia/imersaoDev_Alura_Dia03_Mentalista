64021f6c02120ee270f106ce98c2d54e2d42cff6
Flavio Rezende Ladeia <flavio.ladeia@ebserh.gov.br>
2021-08-19 13:31:56 -0300

    refac

--- javascritp.js
+++ javascritp.js
@@ -1,71 +1,39 @@
-// var secretNumber = parseInt(Math.random() * 10)
-// var trying = 3
-
-// while (trying > 0) {
-//     var kick = parseInt(prompt("Digite um número:"))
-//     if (secretNumber == kick) {
-//         alert("Acertou")
-//         break
-//     } else if (kick < secretNumber) {
-//         if (parseInt(trying - 1) > 0) {
-//             alert("O número secreto é maior\n Você tem mais " + parseInt(trying - 1) + " chace\(s\)")
-//         } else {
-//             alert("Errou! Acabaram suas chaces. O número secreto é " + secretNumber)
-//             break
-//         }
-//     } else if (kick > secretNumber) {
-//         if (parseInt(trying - 1) > 0) {
-//             alert("O número secreto é menor\n Você tem mais " + parseInt(trying - 1) + " chace\(s\)")
-//         } else {
-//             alert("Errou! Acabaram suas chaces. O número secreto é " + secretNumber)
-//             break
-//         }
-//     }
-//     trying--
-// }
-
-
-
-var secretNumber = parseInt(Math.random() * 10 + 1)
-var trying = 3
-var chooseNumber = document.querySelector("input[name=choose]")
-var tipMessage = document.querySelector("#tipMessage")
-
-// chooseNumber.addEventListener("input", function () {
-//     console.log(chooseNumber.value)
-// })
+const secretNumber = parseInt(Math.random() * 10 + 1)
+const chooseNumber = document.querySelector("input[name=choose]")
+const tipMessage = document.querySelector("#tipMessage")
+const numberOfGuests = document.querySelector("#nChoose")
+let trying = 3
+const guests = []
 
 function btnTry() {
-    kick = chooseNumber.value
-    // while (trying > 0) {
-    // var kick = parseInt(prompt("Digite um número:"))
+    const kick = chooseNumber.value
+    guests.push(kick)
+
+    numberOfGuests.innerHTML = guests
+
     if (secretNumber == kick) {
-        tipMessage.innerHTML = `<strong>Acertou!</strong> <br>Parabéns!`
-        // break
-    } else if (kick < secretNumber) {
+        return tipMessage.innerHTML = `<strong>Acertou!</strong> <br>Parabéns!`
+    }  
+    
+    if (kick < secretNumber) {
         if (parseInt(trying - 1) > 0) {
-            tipMessage.innerHTML = `O número secreto é maior <br> Você tem mais  <strong>${parseInt(trying - 1)}</strong> chance\(s\)`
+            tipMessage.innerHTML = `O número secreto é <strong>maior</strong> <br> Você tem mais  <strong>${parseInt(trying - 1)}</strong> chance\(s\)`
         } else {
-            tipMessage.innerHTML = "Acabaram suas chaces. O número secreto é " + secretNumber
-            // break
+            tipMessage.innerHTML = `Acabaram suas chaces. O número secreto é <strong>${secretNumber}</strong>`
         }
     } else if (kick > secretNumber) {
         if (parseInt(trying - 1) > 0) {
-            tipMessage.innerHTML = `O número secreto é menor <br> Você tem mais  <strong>${parseInt(trying - 1)}</strong> chance\(s\)`
+            tipMessage.innerHTML = `O número secreto é <strong>menor</strong> <br> Você tem mais  <strong>${parseInt(trying - 1)}</strong> chance\(s\)`
         } else {
-            tipMessage.innerHTML = `Acabaram suas chaces. O número secreto é  ${secretNumber}`
-            // break
+            tipMessage.innerHTML = `Acabaram suas chaces. O número secreto é <strong>${secretNumber}</strong>`
         }
     }
     trying--
-    // }
+    chooseNumber.value = ''
 }
 
+chooseNumber.addEventListener("input", btnTry)
 
-
-
-// function getRandomArbitrary(min, max) {
-//     return parseInt(Math.random() * (max - min) + 1);
-// }
-
-// console.log(getRandomArbitrary(0, 10))
+function reiniciar() {
+    location.reload()
+}
